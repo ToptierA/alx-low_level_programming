950a7686900d76e04be3ccfe13dfedfde619a3fe
ToptierA <squantowilly@gmail.com>
2023-03-16 23:35:32 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - pirnts "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standard error.
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	return (1)
+}
