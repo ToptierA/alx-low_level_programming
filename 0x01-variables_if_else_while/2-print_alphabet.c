e945aef87e8ab6d1c262e7a55a39dd65f048a6f2
ToptierA <squantowilly@gmail.com>
2023-03-18 15:08:29 -0700

    Convert to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+/**
+ * main - print all alphabets in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char x
+		for(x = 'A'; x <= 'Z'; x++)
+		{
+			x = tolower(x);
+			putchar(x);
+		}
+	puctchar('\n');
+	return (0);
+}
