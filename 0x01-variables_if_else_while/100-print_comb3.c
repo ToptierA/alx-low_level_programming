3fe303f48e2e00387ab304c0a5d24a40a1eada70
ToptierA <squantowilly@gmail.com>
2023-03-19 00:35:50 -0700

    Possible two digits combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 int a, b;
 for (a = 48; a <= 56; a++)
 {
-	for (b = 49; b <=57; b++)
+	for (b = 49; b <= 57; b++)
 	{
 		if (b > a)
 		{
 			putchar(a);
 			putchar(b);
-			if (a !=56 || b != 57)
+			if (a != 56 || b != 57)
 			{
 				putchar(',');
 				putchar(' ');
