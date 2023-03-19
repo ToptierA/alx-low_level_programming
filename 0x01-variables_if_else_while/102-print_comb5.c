e908434872fe2cc0684db87e4f77014dc44b88e9
ToptierA <squantowilly@gmail.com>
2023-03-19 15:12:41 -0700

    Combinations of two number 0 to 100

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - prints all possible combinations of two two-digit numbers
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int w, m;
+
+	for (w = 0; w < 100; w++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (w < m)
+			{
+			putchar((w / 10) + 48);
+			putchar((w % 10) + 48);
+			putchar(' ');
+			putchar((m / 10) + 48);
+			putchar((m % 10) + 48);
+			if (w != 98 || m != 99);
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
