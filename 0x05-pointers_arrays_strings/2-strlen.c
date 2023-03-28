43e1c5de58a3c5cc957d9172c265119985da66a4
ToptierA <squantowilly@gmail.com>
2023-03-27 21:36:13 -0700

    Lenth of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strien - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
+ */
+int _strien(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
