f00f78792ae4fb039f6196922454d8911ef40bd2
ToptierA <squantowilly@gmail.com>
2023-04-03 21:45:14 -0700

    constant memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
-/**_memset - function fills the
- * first @n: bytes of the memory area
- * pointed to by s with the constant byte c.
- * @s: the pointer who's first memory will be filled
- * @n: the byte to be filled to the first pointer
- * @b: the constant to fill the first address
- * Return: always the (s);
+/**_memset - sets memory to array
+ * @s: array to set
+ * @n: n amount of times
+ * @b: value to set it as
+ * Return: char value of s;
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 
 	unsigned int i;
@@ -18,10 +16,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	{
 
-		dest[i] = src[i];
+		s[i] = b;
 
 	}
 
-	return (dest);
+	return (s);
 
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,53 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_number(int n);
-int _isupper(int c);/*finds the uppercase letters*/
-int _isdigit(int c);
-void print_triangle(int size);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void print_line(int n);
-void print_diagonal(int size);
-void print_square(int size);
-void more_numbers(void);
 int _putchar(char c);
-void print_alphabet_x10();
-int _islower();
-int _isalpha();
-int _abs();
-int print_sign();
-int print_last_digit();
-void jack_bauer();
-void times_table();
-int add(int num1, int num2);
-void print_to_98();
-
-/*0x05-pointers_arrays_strings*/
-void swap_int(int *a, int *b);
-void reset_to_98(int *n);
-void _puts(char *str);
-int _strlen(char *s);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-
-/*06*/
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *rot13(char *);
-void *print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-
-#endif /*MAIN_H*/
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
