0cb29fe396be41e6a57e3076b2efe596bb682e1e
ToptierA <squantowilly@gmail.com>
2023-04-09 09:11:25 -0700

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,33 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
- * is_prime - recursively divide by higher divisor, skip even nums
- * @n: number to check if prime
- * @divisor: divisor
- * Return: 1 if prime, 0 if not, or recursive function call
+ * is_palindrome - a function that returns 1
+ * if a string is a palindrome and 0 if not.
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
  */
 
-int is_prime(int n, int divisor)
+int is_palindrome(char *s)
 {
-	if (n == divisor)
+	if(*s == 0)
 		return (1);
-	if (n % divisor == 0)
-		return (0);
-	return (is_prime(n, divisor + 1));
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * is_prime_number - check if prime
- * @n: number to check
- * Return: 1 if prime, 0 if not
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
  */
 
-int is_prime_number(int n)
+int _strlen_recursion(char *s)
 {
-	int divisor = 3;
-
-	if (n % 2 == 0 || n < 2)
+	if (*s == '\0')
 		return (0);
-	if (n == 2)
-		return (1);
-
-	return (is_prime(n, divisor));
+	return (1 + _strlen_recursion(s + 1));
 }
