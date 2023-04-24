e8c2c414a860df22177d9d0051996d00a40bac5d
ToptierA <squantowilly@gmail.com>
2023-04-24 02:03:13 -0700

    file compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
