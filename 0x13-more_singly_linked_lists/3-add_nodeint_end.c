e8a24cb93e9f217a297ff461c5f45bb2a4233665
ToptierA <squantowilly@gmail.com>
2023-05-02 11:17:13 -0700

    Adds a new node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new;
-		return (new);
+		return (*head);
 	}
 	temp = *head;
 	while (temp->next != NULL)
@@ -28,5 +28,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp = temp->next;
 	}
 	temp->next = new;
-	return (new);
+	return (*head);
 }
